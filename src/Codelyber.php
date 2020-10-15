c1a04978acff2f5dd486f6bbcb15748319319a69
Rafa Gómez <rgomezcasas@gmail.com>
2020-10-15 18:04:20 +0200

    Initial commit

---
+++ src/Codelyber.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodelyTv;
+
+final class Codelyber
+{
+    private const GREETING = "CodelyTV";
+    private string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function greet(): string
+    {
+        return self::GREETING;
+    }
+}
